3b130800db643a349c3288619fed8aee236cb587
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2021-06-24 22:16:04 +0300

    Підготував проєкт для написання класу RFM69

---
+++ STM32F051_RFM69_ATC/Debug/uart/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../uart/uart.cpp 
+
+OBJS += \
+./uart/uart.o 
+
+CPP_DEPS += \
+./uart/uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+uart/uart.o: ../uart/uart.cpp uart/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m0 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F051x8 -c -I../Core/Inc -I"C:/STM32 workspace/STM32 RFM69 SPI TxRx/STM32F051_RFM69_ATC/spi" -I"C:/STM32 workspace/STM32 RFM69 SPI TxRx/STM32F051_RFM69_ATC/uart" -I"C:/STM32 workspace/STM32 RFM69 SPI TxRx/STM32F051_RFM69_ATC/gpio" -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"uart/uart.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
